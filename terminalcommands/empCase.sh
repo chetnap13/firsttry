41bc8c91e129f120507a7210035e0c72cec1a16f
chetna <cchetnapatil@gmail.com>
2020-06-25 11:47:29 +0530

    first commit

---
+++ terminalcommands/empCase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+ratePerHour=20;
+emprandomCheck=$(( RANDOM%3 ))
+case $emprandomCheck in
+	$isFullTime)
+		empHours=8;
+		;;
+	 $isPartTime)
+        	empHours=4;
+		;;
+		*)
+        	empHours=0;
+		;;
+esac
+salary=$(( $ratePerHour * $empHours ))
+echo $salary
+
